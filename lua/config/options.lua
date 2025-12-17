24d4674446cf9f6e48dc730ba0e5c2d78e54f2aa
Lucas Schlindwein <lucas.schlindwein@jaspe.aprimorar.net.br>
2025-12-17 14:30:02 -0300

    feat: creates options.lua in config folder for identation setup

---
+++ lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Tabs e indentação
+vim.opt.tabstop = 4      -- quantos espaços um <Tab> aparenta
+vim.opt.shiftwidth = 4   -- quantos espaços ao indentar (>>, <<)
+vim.opt.softtabstop = 4  -- quantos espaços ao pressionar <Tab>
+vim.opt.expandtab = true -- converte <Tab> em espaços
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,27 +1,27 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "v0.2.0",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "v0.2.0",
+        dependencies = {
+        "nvim-lua/plenary.nvim",
     },
     config = function()
-      local telescope = require("telescope")
-      telescope.setup({
-        defaults = {
-          file_ignore_patterns = {
-            "node_modules",
-            ".git/",
-          },
-        },
-      })
+        local telescope = require("telescope")
+        telescope.setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    ".git/",
+                },
+            },
+        })
 
-      local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'Telescope help tags' })
+        local builtin = require("telescope.builtin")
+        vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>tg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'Telescope help tags' })
     end,
-  },
+    },
 }
 
